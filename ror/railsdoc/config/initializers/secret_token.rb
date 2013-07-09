493fc12183eda874023c38eb57f34aae06843697
luongphuc <luong.phuc@mulodo.com>
2013-07-09 15:34:53 +0700

    Update railsdoc

---
+++ ror/railsdoc/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsdoc::Application.config.secret_token = '853dc11b81c3f8f84ba4efc47fffbc5de5a97ae667396c90d067b569f8c55728c93250c598251ab499ff5d81810c45e15ef67d901737ffef9b4f8ed2484140c9'
