86653e69db43a5df8f2056cc84a849f50806fd58
nguyen-hoang-mulodo <nguyen.hoang@mulodo.com>
2013-07-25 17:09:45 +0700

    spec helper blog

--- ror/railsdoc/spec/helpers/blogs_helper_spec.rb
+++ ror/railsdoc/spec/helpers/blogs_helper_spec.rb
@@ -11,5 +11,32 @@ require 'spec_helper'
 #   end
 # end
 describe BlogsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #include UsersHelper
+
+  describe "#bool" do
+    it "return true or false" do
+      helper.bool.should be_true
+    end
+
+    it "return name default" do
+      expect(helper.nameHelperDefault).to eq("RSpec is your friend")
+    end
+  end
+
+  describe "#page_title" do
+    it "returns the instance variable" do
+      assign(:title, "My Title")
+      helper.page_title.should eql("My Title")
+    end
+  end
+
+  describe "require_getinfo" do
+
+    it ".No_Session" do
+      session[:user_id] = nil
+      expect(session[:user_id]).to eq nil
+      expect(helper.require_getinfo).to eq false
+    end
+  end
+
 end
