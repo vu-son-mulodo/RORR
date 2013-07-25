a51e11545bb7e54b7fc2ca8abfa73c6fd2410c08
nguyen-hoang-mulodo <nguyen.hoang@mulodo.com>
2013-07-25 17:10:13 +0700

    spec model blog

--- ror/railsdoc/spec/models/blog_spec.rb
+++ ror/railsdoc/spec/models/blog_spec.rb
@@ -11,9 +11,16 @@ describe Blog do
 	end
 
 	it ".validate_max(255)" do
-	  expect(Blog.create(:title => "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccc")).to have(1).error_on(:title, :context => :create)
+	  expect(Blog.create(:title => "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbcccccc")).to have(1).error_on(:title, :context => :create)
 	end
 
+  end
+
+  context :update do
+	it "fails validation with no title (using error_on)" do
+    expect(Blog.new).to have(1).error_on(:title)
+    expect(Blog.new).to have(1).error_on(:content)
+  end
   end
 
 end
