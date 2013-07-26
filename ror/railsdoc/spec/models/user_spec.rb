b2f39cc2c5b68c23e71492f1b7eec4ace2452876
luong-phuc-mulodo <luong.phuc@mulodo.com>
2013-07-26 16:47:34 +0700

    update code

--- ror/railsdoc/spec/models/user_spec.rb
+++ ror/railsdoc/spec/models/user_spec.rb
@@ -3,16 +3,6 @@ require "spec_helper"
 describe Users do
 
   before(:each) do
-=begin
-    @account = {
-	  :fullname => "Lương Ngọc Minh Phúc",
-	  :username => "minhphuc86",
-	  :password => "t4Js40!#%^!@$",
-	  :password_confirmation => "t4Js40!#%^!@$"
-	}
-
-    @minhphuc = Users.find_by_username(@account[:username])
-=end
 	@minhphuc = FactoryGirl.create(:tranvinh)
 	@account = FactoryGirl.build(:tranvinh)
   end
