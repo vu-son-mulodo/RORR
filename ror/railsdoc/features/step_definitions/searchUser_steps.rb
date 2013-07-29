3242e4e08fadc276084a07c63090b8708e61403e
tran-vinh-mulodo <tran.vinh@mulodo.com>
2013-07-29 16:13:07 +0700

    search content page

--- ror/railsdoc/features/step_definitions/searchUser_steps.rb
+++ ror/railsdoc/features/step_definitions/searchUser_steps.rb
@@ -1,5 +1,12 @@
 Then(/^I see "(.*?)" on page$/) do |arg1|
+  puts('number of rows: '+page.all('table#rounded-corner tbody tr').count.to_s)
   page.should have_content arg1
+  
+  
+  #page.should have_selector("a[href='http://thoughtbot.com']")
+  page.all('table#rounded-corner tbody').each { |a| 
+    puts(a.text)
+  }
 end
 
 Given(/^I visit "(.*?)" view list user$/) do |arg1|
