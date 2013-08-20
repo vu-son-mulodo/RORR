df2c3031b3c596d05d797671e1f165c05fc8b634
luong-phuc-mulodo <luong.phuc@mulodo.com>
2013-08-20 13:39:28 +0700

    Update format code

--- luong.phuc/ruby1.rb
+++ luong.phuc/ruby1.rb
@@ -1,50 +1,50 @@
 class Cell
-	NO_CHECK = false
-	IS_CHECKED = true
-	IS_LAND = 1
-	IS_CELL = 0
+  NO_CHECK = false
+  IS_CHECKED = true
+  IS_LAND = 1
+  IS_CELL = 0
 
-	attr_reader :island, :checked
+  attr_reader :island, :checked
 
   def initialize
-		@island,@checked = rand(2),NO_CHECK
+	@island,@checked = rand(2),NO_CHECK
   end
 
   def is_checked
-		@checked = IS_CHECKED
+	@checked = IS_CHECKED
   end
 
-end
+  end
 
-class Sea
-	attr_reader :status, :cells, :row, :column
+  class Sea
+  attr_reader :status, :cells, :row, :column
 
   def initialize(row, column)
-		raise ArgumentError, ' m and n should be numberic and more than 2' unless (row.is_a? Numeric) && (column.is_a? Numeric) && (row > 2) && (column > 2)
-		@status,@row,@column,@cells = true,row,column,[]
-		set_islands_at_random
-	end
+	raise ArgumentError, ' m and n should be numberic and more than 2' unless (row.is_a? Numeric) && (column.is_a? Numeric) && (row > 2) && (column > 2)
+	@status,@row,@column,@cells = true,row,column,[]
+	set_islands_at_random
+  end
 
   def set_islands_at_random
-		draw unless (@cells = (0...@row).map{|i| (0...@column).map{|j| Cell.new}}).nil?
+	draw unless (@cells = (0...@row).map{|i| (0...@column).map{|j| Cell.new}}).nil?
   end
 
   def draw
-		(0...@row).each {|i| puts (0...@column).map{|j| "#{@cells[i][j].island}"}.join(' ') }
-	end
+	(0...@row).each {|i| puts (0...@column).map{|j| "#{@cells[i][j].island}"}.join(' ') }
+  end
 
   def count_island
-		(0...@row).map{|i| (0...@column).map{|j| check_island(i,j)}}.flatten.select{|x| x==true }.size
+	(0...@row).map{|i| (0...@column).map{|j| check_island(i,j)}}.flatten.select{|x| x==true }.size
   end
 
   def check_island(x,y)
-		return false unless (@cells[x][y].island === Cell::IS_LAND) && (@cells[x][y].checked === Cell::NO_CHECK)
-		@cells[x][y].is_checked
-		check_island(x,y+1) if y+1 < @column
-		check_island(x,y-1) if y-1 >= 0
-		check_island(x+1,y) if x+1 < @row
-		check_island(x-1,y) if x-1 >= 0
-		true
+	return false unless (@cells[x][y].island === Cell::IS_LAND) && (@cells[x][y].checked === Cell::NO_CHECK)
+	@cells[x][y].is_checked
+	check_island(x,y+1) if y+1 < @column
+	check_island(x,y-1) if y-1 >= 0
+	check_island(x+1,y) if x+1 < @row
+	check_island(x-1,y) if x-1 >= 0
+	true
   end
 
 end
@@ -52,7 +52,7 @@ end
 sea = Sea.new(5,5)
 count = sea.count_island
 if count > 0
-  puts "\n There are #{count} island in this map"
+puts "\n There are #{count} island in this map"
 else
-  puts "\n There is not island in this map"
+puts "\n There is not island in this map"
 end
--- luong.phuc/ruby2.rb
+++ luong.phuc/ruby2.rb
@@ -1,27 +1,27 @@
 class Test2
-	attr_reader :a, :b, :num, :list
+  attr_reader :a, :b, :num, :list
 
   def initialize(a, b)
-		raise ArgumentError, ' a and b should be numberic and more than 2' unless (a.is_a? Numeric) && (b.is_a? Numeric)
-		@a,@b = a,b
+	raise ArgumentError, ' a and b should be numberic and more than 2' unless (a.is_a? Numeric) && (b.is_a? Numeric)
+	@a,@b = a,b
   end
 
   def required_num=(num)
-		@num = num if (num.is_a? Numeric)
+	@num = num if (num.is_a? Numeric)
   end
 
   def run
-		return nil if @num.nil?
-		puts (@list = (@a..@b).select{|x| check_num(x) }).size
+	return nil if @num.nil?
+	puts (@list = (@a..@b).select{|x| check_num(x) }).size
   end
 
   def check_num(x)
-		temp = x.to_s.split(//)
-		(sum(temp[0].to_i,temp[1].to_i) + sum(temp[2].to_i,temp[3].to_i)) == @num ? true : false
+	temp = x.to_s.split(//)
+	(sum(temp[0].to_i,temp[1].to_i) + sum(temp[2].to_i,temp[3].to_i)) == @num ? true : false
   end
 
   def sum(x,y)
-		(x+y)%10
+	(x+y)%10
   end
 
 end
